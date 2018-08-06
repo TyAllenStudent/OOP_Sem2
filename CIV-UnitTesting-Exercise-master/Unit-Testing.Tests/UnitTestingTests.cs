8d3dab6d9d4c12cf77e9316743f6e9ba0fb6b980
Ty Allen <102058205@student.swin.edu.au>
2018-08-06 16:29:42 +1000

    Initial Commit

---
+++ CIV-UnitTesting-Exercise-master/Unit-Testing.Tests/UnitTestingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Unit_Testing.Tests
+{
+    [TestFixture]
+    class UnitTestingTests
+    {
+        [TestCase]
+        int FibocacciRec(int num1, int num2, int numFibs)
+        {
+            if (numFibs > 0)
+            {
+                numFibs--;
+                num2 = num1 + num2;
+                return FibocacciRec(num2, num2 + num1, numFibs);
+
+                
+            }
+
+            Console.WriteLine(numFibs);
+
+            return num2 + num1;
+            
+        }
+        
+    }
+}
